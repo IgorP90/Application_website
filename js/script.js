6b3dd2791684ab247151a1fada0425685894464c
IgorP <IgorP90@mail.ru>
2023-06-05 20:47:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let etext = document.querySelector('.ee')
+let wtext = document.querySelector('.ww')
+ 
+const b1 = document.querySelector('.b1')
+const b2 = document.querySelector('.b2')
+const b3 = document.querySelectorAll('b3')
+
+b1.onclick = function(){
+    console.log(etext.value);
+    
+ }
+
+b2.onclick = function(){
+    etext.select();
+ }
+
+b3.onclick = function(){
+    
+ } 
+
+let f1 = function(){
+    let res = '/' + wtext +'/'
+}
+
+
+ 
+
