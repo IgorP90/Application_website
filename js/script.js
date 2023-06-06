687c209eea41022c1424fcd922d9007779100f31
IgorP <IgorP90@mail.ru>
2023-06-06 21:07:23 +0300

    Web application development HTML: Changed and renamed some tags. JS: Functions have been written or modified to handle some events.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
-let etext = document.querySelector('.ee')
-let wtext = document.querySelector('.ww')
+let etext = document.querySelector('#inputText_textarea')
+let wtext = document.querySelector('.findText_input')
+let qtext = document.querySelector('.replacementText_input')
  
-const b1 = document.querySelector('.b1')
-const b2 = document.querySelector('.b2')
-const b3 = document.querySelectorAll('b3')
+document.querySelector('.find_btn').onclick = function(){
+    etext.value = etext.textContent.replace(new RegExp(wtext.value,'gi'), wtext.innerHTML = /!/+wtext.value+/!/)
 
-b1.onclick = function(){
-    console.log(etext.value);
-    
- }
-
-b2.onclick = function(){
-    etext.select();
- }
-
-b3.onclick = function(){
-    
- } 
-
-let f1 = function(){
-    let res = '/' + wtext +'/'
 }
+
+document.querySelector('.b2').onclick = function(){
+    etext.select()
+}
+
+document.querySelectorAll('replace_btn').onclick = function(){
+    etext.value = etext.textContent.replace(new RegExp(wtext.value,'gi'), wtext.innerHTML = qtext.value)
+}
+
+// b1.onclick = function(){
+//     console.log(etext.value)
+//      console.log(f1())
+//  }
+
+// b2.onclick = function(){
+//     etext.select()
+//  }
+
+// b3.onclick = function(){
+    
+//  } 
+
+// const f1 = function(){
+//     let regexp = new RegExp(wtext.value, 'gi')
+//     regexp.select()
+//     return regexp
+// }
+
 
 
  
