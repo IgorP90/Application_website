51ea9d1b506e8899cf462058f284a295606bd302
IgorP90 <IgorP90@mail.ru>
2023-06-13 21:38:56 +0300

    Web application development HTML: The text input field has been changed. Renamed several tag attributes. CSS: Added inputText_p element. JS: Redesigned findText_input function.   Added toLetterCase_btn function. Changes have been made to all other functions. Added text input field background.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-const etext = document.querySelector('#inputText_textarea')
+const etext = document.querySelector('.inputText_p')
 const wtext = document.querySelector('.findText_input')
 const qtext = document.querySelector('.replacementText_input')
  
-document.querySelector('.find_btn').onclick = function(){
-    etext.value = etext.textContent.replace(new RegExp(wtext.value,'gi'), `<${wtext.value}>`)
+document.querySelector('.findText_input').oninput = function(){
+    //let r = new RegExp(wtext.value,'gi')    
+    etext.innerHTML = etext.innerHTML.replace(new RegExp(wtext.value,'gi','<mark>'+wtext.value+'</mark>'))
 }
 
 document.querySelector('.select_all_btn').onclick = function(){  //+
@@ -11,21 +12,38 @@ document.querySelector('.select_all_btn').onclick = function(){  //+
 }
 
 document.querySelector('.replace_btn').onclick = function(){ //+
-    etext.value = etext.textContent.replace(new RegExp(wtext.value,'gi'), qtext.value)
+    etext.innerHTML = etext.innerHTML.replace(new RegExp(wtext.value,'gi'), qtext.value)
 }
 
 document.querySelector('.countAllChars_btn').onclick = function(){ //+
     const check = document.querySelector('#spaceCheckbox_input')
     if(!check.checked){
-        let s = etext.value.match(/\S/g).length
+        let s = etext.innerHTML.match(/\S/g).length
         console.log('Число символов - ' + s)
     }
     else{
-        let s = etext.value.length
+        let s = etext.innerHTML.length
         console.log('Число символов - ' + s)
     }  
 }
 
 document.querySelector('.cut_btn').onclick = function(){ //+
-etext.value = etext.textContent.replace(new RegExp(wtext.value,'gi'),"")
+    etext.innerHTML = etext.innerHTML.replace(new RegExp(wtext.value,'gi'),"")
 }
+
+document.querySelector('.toLetterCase_btn').onclick = function(){
+    let check = document.getElementsByName('toLetterCase')
+    for(const c of check){
+        if(c.value == 'toLetterCaseHeadwords' && c.checked){
+            
+        }
+        else if(c.value == 'toLetterCaseUppercase' && c.checked){
+            etext.innerHTML = etext.innerHTML.toUpperCase();
+        }  
+        else if(c.value == 'toLetterCaseLowercase' && c.checked){
+            etext.innerHTML = etext.innerHTML.toLowerCase();
+        } 
+    }
+}
+
+
