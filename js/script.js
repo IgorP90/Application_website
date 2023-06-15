970424da1031f07beac9a3f055fd1dfaaf853afd
IgorP90 <IgorP90@mail.ru>
2023-06-15 21:01:12 +0300

    Web application development index.html: Changed forward and backward buttons. Added choice of font style. Changed file extension selection tags. style.css: Added fonts. Added page background. Changed forward and backward buttons. Changed the background on the middle block. script.js: The findText_input function has been changed. Added function fontFamily_select, deleteExtraSpaces_btn, back_btn, forward_btn, backup, extension_select and save_btn. Added folder with fonts and some images.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ const etext = document.querySelector('.inputText_p')
 const wtext = document.querySelector('.findText_input')
 const qtext = document.querySelector('.replacementText_input')
  
-document.querySelector('.findText_input').oninput = function(){ //+
+document.querySelector('.findText_input').oninput = function(){ //-
     etext.innerHTML = etext.innerHTML.replace( /(<([^>]+)>)/ig, '')
     if(wtext.value != ''){
         etext.innerHTML = etext.innerHTML.replace(RegExp(wtext.value,'ig'),'<mark>$&</mark>')
+    }
+    else if(wtext.value == '/'){
+        console.log('это рег выражение')
     }
 }
     
@@ -16,6 +19,7 @@ document.querySelector('.select_all_btn').onclick = function(){  //-
 
 document.querySelector('.replace_btn').onclick = function(){ //+
     etext.innerHTML = etext.innerHTML.replace(new RegExp(wtext.value,'gi'), qtext.value)
+    backup()
 }
 
 document.querySelector('.countAllChars_btn').onclick = function(){ //+
@@ -32,6 +36,7 @@ document.querySelector('.countAllChars_btn').onclick = function(){ //+
 
 document.querySelector('.cut_btn').onclick = function(){ //+
     etext.innerHTML = etext.innerHTML.replace(new RegExp(wtext.value,'gi'),"")
+    backup()
 }
 
 document.querySelector('.toLetterCase_btn').onclick = function(){ //-
@@ -47,8 +52,95 @@ document.querySelector('.toLetterCase_btn').onclick = function(){ //-
             etext.innerHTML = etext.innerHTML.toLowerCase();
         } 
     }
+    backup()
+}
+
+document.querySelector('#fontFamily_select').oninput = function(){ //+
+    const fontFamilies = document.querySelector('#fontFamily_select')
+    if(fontFamilies.options[fontFamilies.selectedIndex].value == 'josephina_FlourishesC'){
+        etext.style.fontFamily = 'Josephina FlourishesC'
+    }
+    else if(fontFamilies.options[fontFamilies.selectedIndex].value == 'theano_Didot'){
+        etext.style.fontFamily = 'Theano Didot'
+    }
+    
 }
 
 document.querySelector('#fontSize_input').oninput = function(){ //+
     etext.style.fontSize = document.querySelector('#fontSize_input').value+"px"
 }
+
+document.querySelector('.deleteExtraSpaces_btn').onclick = function(){
+    let r = /\s+/g
+    etext.innerHTML = etext.innerHTML.replace(r,'')
+}
+//#region Backup
+let ar= []
+let i = 0
+
+document.querySelector('.back_btn').onclick = function(){
+    i--
+    if(i<=0)i=0
+    etext.innerHTML = ar[i]
+    console.log('i = '+i)
+}
+
+document.querySelector('.forward_btn').onclick = function(){
+    i++
+    if(i>ar.length)i=ar.length
+    etext.innerHTML = ar[i]
+    console.log('i = '+i)
+}
+
+function backup(){   
+    if(i == ar.length){
+        ar.push(etext.innerHTML)
+        i = ar.length
+    }
+    else{
+        ar[i] = etext.innerHTML
+        for (let j = i; j < ar.length; j++) {
+            ar.splice(j+1)
+        }
+    }
+    // console.log('ar = '+ar)
+    // console.log('длинна ar = '+ar.length)
+    // console.log('i = '+i)
+}
+backup()
+//#endregion
+//#region Save
+let extension = ''
+
+document.querySelector('#extension_select').oninput = function(){
+    const extensions = document.querySelector('#extension_select')
+    if(extensions.options[extensions.selectedIndex].value == 'txt'){
+        console.log('txt')    
+        extension = '.txt'
+    }
+    else if(extensions.options[extensions.selectedIndex].value == 'pdf'){
+        console.log('pdf')
+        extension = '.pdf'
+    }
+    else if(extensions.options[extensions.selectedIndex].value == 'rtf'){
+        console.log('rtf')
+        extension = '.rtf'
+    }
+}
+
+// function save(){
+//     window.showSaveFilePicker(etext.innerHTML);
+// }
+
+document.querySelector('.save_btn').onclick = async function getNewFileHandle() {
+    const opts = {
+      types: [
+        {
+          description: "Text file",
+          accept: { "text/plain": [extension] },
+        },
+      ],
+    };
+    return await window.showSaveFilePicker(opts);
+  }
+//#endregion
